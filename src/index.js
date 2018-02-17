bd7e25ef95b1b687c75a9196a39d955152327711
github username <github email address>
2018-02-17 14:03:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const paragraph = React.createElement('p', {}, 'Who is learning React');
+const list = React.createElement('ul', {className: 'my-interests'},
+      [
+        React.createElement('li', {}, 'JavaScript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'Ice cream')
+      ]
+    )
+const meInReact = React.createElement('div', {className: 'me'}, [title, paragraph, list]);
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
+
+// const meInReact =
+//   React.createElement('div', {className: 'me'},
+//     React.createElement('h1', {}, 'An Awesome Person'),
+//     React.createElement('p', {}, 'Who is learning React'),
+//     React.createElement('ul', {className: 'my-interests'},
+//       [
+//         React.createElement('li', {}, 'JavaScript'),
+//         React.createElement('li', {}, 'React'),
+//         React.createElement('li', {}, 'Movies'),
+//         React.createElement('li', {}, 'Ice Cream')
+//       ]
+//     )
+//   );
+//
+// ReactDOM.render(
+//   meInReact,
+//   document.getElementById('global')
+// );
+
+// <div class="me">
+//   <h1>An Awesome Person</h1>
+//   <p>Who is learning React</p>
+//
+//   <ul class="my-interests">
+//     <li>JavaScript</li>
+//     <li>React</li>
+//     <li>Movies</li>
+//     <li>Ice cream</li>
+//   </ul>
+// </div>
